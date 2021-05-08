97a9d83a1e0f6b38782e0ccb181ec998bfc4cc78
nathan-louie <nathanhlouie@gmail.com>
2021-05-08 17:26:37 -0400

    add header, features, and footer

--- src/App.js
+++ src/App.js
@@ -12,10 +12,37 @@ function App() {
   return (
     <SnackbarProvider>
       <div className="App">
-        <div className="chessboard">
-          <Chessboard position={startPos} />
+        <div className="header">
+          <img src="gofisch.png" alt="Go Fisch" width="150" />
+          <p>An easy way to start playing FischerRandom/Chess960 chess.</p>
         </div>
-        <Actions startPos={startPos} setStartPos={setStartPos} />
+        <div className="main-actions">
+          <div className="chessboard">
+            <Chessboard position={startPos} />
+          </div>
+          <Actions startPos={startPos} setStartPos={setStartPos} />
+        </div>
+        <div className="footer">
+          <p>
+            Made by{" "}
+            <a
+              href="https://nathan.louie.ca/"
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              Nathan
+            </a>{" "}
+            and{" "}
+            <a
+              href="https://emily.louie.ca"
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              Emily
+            </a>{" "}
+            Louie
+          </p>
+        </div>
       </div>
     </SnackbarProvider>
   );
--- src/components/Actions.jsx
+++ src/components/Actions.jsx
@@ -1,8 +1,15 @@
 import React from "react";
+import { useState } from "react";
 import { useSnackbar } from "react-simple-snackbar";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faClipboard } from "@fortawesome/free-regular-svg-icons";
+import { faRandom, faChessKnight } from "@fortawesome/free-solid-svg-icons";
 
 export default function Actions(props) {
   const [openSnackbar] = useSnackbar();
+  const [lichessLink, setLichessLink] = useState(
+    "https://lichess.org/analysis"
+  );
 
   const getStartPos = () => {
     let rank = new Array(8),
@@ -27,12 +34,23 @@ export default function Actions(props) {
       rank[emptySquares()[0]] = x === 2 ? "K" : "R";
     }
     return rank;
-  }
+  };
 
   const randomizeStartPos = () => {
     let str = getStartPos().join("");
-    let newStartPos = str.toLowerCase() + "/pppppppp/8/8/8/8/PPPPPPPP/" + str + " w KQkq - 0 1";
+    let newStartPos =
+      str.toLowerCase() + "/pppppppp/8/8/8/8/PPPPPPPP/" + str + " w KQkq - 0 1";
     props.setStartPos(newStartPos);
+
+    let prms = new URLSearchParams({
+      rated: false,
+      "clock.limit": 10800,
+      "clock.increment": 60,
+      variant: "standard",
+      fen: newStartPos,
+    });
+
+    makeLichessLink(prms);
   };
 
   const handleOnFocus = (event) => {
@@ -45,16 +63,60 @@ export default function Actions(props) {
     navigator.clipboard.writeText(props.startPos);
   };
 
+  const handleLichess = (e) => {
+    e.preventDefault();
+    window.open(lichessLink, "_blank");
+  };
+
+  const makeLichessLink = async (data) => {
+    const requestOptions = {
+      method: "POST",
+      headers: { "Content-Type": "application/x-www-form-urlencoded" },
+      body: data,
+    };
+    const response = await fetch(
+      `https://lichess.org/api/challenge/open`,
+      requestOptions
+    );
+    const responseJson = await response.json();
+    if (responseJson) {
+      setLichessLink(responseJson.challenge.url);
+      console.log(responseJson);
+    }
+  };
+
   return (
-    <div>
-      <button onClick={randomizeStartPos}>uwu</button>
-      <input
-        value={props.startPos}
-        readOnly
-        onFocus={handleOnFocus}
-        onClick={handleOnFocus}
-      />
-      <button onClick={handleCopyAndAlert}>owo</button>
+    <div className="actions">
+      <div className="main-group">
+        <button onClick={randomizeStartPos} className="main-button">
+          Randomize
+          <FontAwesomeIcon style={{ "padding-left": "8px" }} icon={faRandom} />
+        </button>
+
+        <button onClick={handleLichess} className="main-button">
+          Play on Lichess
+          <FontAwesomeIcon
+            style={{ "padding-left": "8px" }}
+            icon={faChessKnight}
+          />
+        </button>
+      </div>
+
+      <div className="copy-group">
+        <label>Take this position somewhere else!</label>
+        <div className="copy-link">
+          <input
+            className="fen-field"
+            value={props.startPos}
+            readOnly
+            onFocus={handleOnFocus}
+            onClick={handleOnFocus}
+          />
+          <button onClick={handleCopyAndAlert} className="copy-button">
+            <FontAwesomeIcon icon={faClipboard} />
+          </button>
+        </div>
+      </div>
     </div>
   );
 }
